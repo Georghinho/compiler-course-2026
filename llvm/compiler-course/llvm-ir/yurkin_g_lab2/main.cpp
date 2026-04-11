46f3b58f3369630783caf69af2c82142e2238be4
georghinho <yurkin23egor@yandex.ru>
2026-04-12 02:48:20 +0300

    try 3

--- llvm/compiler-course/llvm-ir/yurkin_g_lab2/main.cpp
+++ llvm/compiler-course/llvm-ir/yurkin_g_lab2/main.cpp
@@ -1,5 +1,6 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -16,50 +17,48 @@ struct ExamplePass : PassInfoMixin<ExamplePass> {
       for (auto It = BB.begin(); It != BB.end();) {
         Instruction &I = *It++;
 
-        // Floating-point remainder: frem -> a - (fdiv a, b) * b
-        if (auto *FR = dyn_cast<FRemInst>(&I)) {
-          IRBuilder<> B(FR);
-          Value *A = FR->getOperand(0);
-          Value *Bv = FR->getOperand(1);
+        // Проверяем opcode вместо конкретного класса инструкции.
+        if (I.getOpcode() == Instruction::FRem) {
+          IRBuilder<> B(&I);
+          Value *A = I.getOperand(0);
+          Value *Bv = I.getOperand(1);
 
           Value *Div = B.CreateFDiv(A, Bv, "frem.div");
           Value *Mul = B.CreateFMul(Div, Bv, "frem.mul");
           Value *Sub = B.CreateFSub(A, Mul, "frem.sub");
 
-          FR->replaceAllUsesWith(Sub);
-          FR->eraseFromParent();
+          I.replaceAllUsesWith(Sub);
+          I.eraseFromParent();
           Changed = true;
           continue;
         }
 
-        // Signed integer remainder: srem -> a - (sdiv a, b) * b
-        if (auto *SR = dyn_cast<SRemInst>(&I)) {
-          IRBuilder<> B(SR);
-          Value *A = SR->getOperand(0);
-          Value *Bv = SR->getOperand(1);
+        if (I.getOpcode() == Instruction::SRem) {
+          IRBuilder<> B(&I);
+          Value *A = I.getOperand(0);
+          Value *Bv = I.getOperand(1);
 
           Value *Div = B.CreateSDiv(A, Bv, "srem.sdiv");
           Value *Mul = B.CreateMul(Div, Bv, "srem.mul");
           Value *Sub = B.CreateSub(A, Mul, "srem.sub");
 
-          SR->replaceAllUsesWith(Sub);
-          SR->eraseFromParent();
+          I.replaceAllUsesWith(Sub);
+          I.eraseFromParent();
           Changed = true;
           continue;
         }
 
-        // Unsigned integer remainder: urem -> a - (udiv a, b) * b
-        if (auto *UR = dyn_cast<URemInst>(&I)) {
-          IRBuilder<> B(UR);
-          Value *A = UR->getOperand(0);
-          Value *Bv = UR->getOperand(1);
+        if (I.getOpcode() == Instruction::URem) {
+          IRBuilder<> B(&I);
+          Value *A = I.getOperand(0);
+          Value *Bv = I.getOperand(1);
 
           Value *Div = B.CreateUDiv(A, Bv, "urem.udiv");
           Value *Mul = B.CreateMul(Div, Bv, "urem.mul");
           Value *Sub = B.CreateSub(A, Mul, "urem.sub");
 
-          UR->replaceAllUsesWith(Sub);
-          UR->eraseFromParent();
+          I.replaceAllUsesWith(Sub);
+          I.eraseFromParent();
           Changed = true;
           continue;
         }
