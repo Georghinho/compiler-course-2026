7666cf089c9399af06f062f539ab636be7d8edef
georghinho <yurkin23egor@yandex.ru>
2026-05-10 14:05:52 +0300

    3rd try

--- mlir/compiler-course/yurkin_g_lab4/ExamplePass.cpp
+++ mlir/compiler-course/yurkin_g_lab4/ExamplePass.cpp
@@ -12,73 +12,84 @@ using namespace mlir;
 namespace {
 class ExamplePass : public PassWrapper<ExamplePass, OperationPass<ModuleOp>> {
 public:
-  StringRef getArgument() const final { return "example_MLIR"; }
-  StringRef getDescription() const final { return "Description pass"; }
+  StringRef getArgument() const final { return "yurkin-condition-tracer"; }
+  StringRef getDescription() const final {
+    return "Insert trace calls for condition blocks";
+  }
 
   void runOnOperation() override {
     ModuleOp module = getOperation();
-    OpBuilder moduleBuilder(module.getContext());
+    MLIRContext *ctx = module.getContext();
+    OpBuilder moduleBuilder(ctx);
     moduleBuilder.setInsertionPointToStart(&module.getBodyRegion().front());
     auto getOrCreateFunc = [&](StringRef name) -> func::FuncOp {
       if (auto f = module.lookupSymbol<func::FuncOp>(name))
         return f;
       OpBuilder::InsertionGuard g(moduleBuilder);
       moduleBuilder.setInsertionPointToStart(&module.getBodyRegion().front());
-      FunctionType funcType = FunctionType::get(module.getContext(), {}, {});
+      FunctionType funcType = FunctionType::get(ctx, {}, {});
       return moduleBuilder.create<func::FuncOp>(module.getLoc(), name,
                                                 funcType);
     };
     for (auto ifOp : module.getOps<scf::IfOp>()) {
       Location loc = ifOp.getLoc();
-      if (ifOp.thenRegion().empty() == false) {
-        Block &thenBlock = ifOp.thenRegion().front();
+      Region &thenReg = ifOp.getThenRegion();
+      if (!thenReg.empty()) {
+        Block &thenBlock = thenReg.front();
         OpBuilder b(&thenBlock, thenBlock.begin());
         func::FuncOp fbegin = getOrCreateFunc("trace_condition_then_begin");
-        b.create<func::CallOp>(loc, fbegin.getSymName(), ArrayRef<Type>{},
+        b.create<func::CallOp>(loc, fbegin.getName(), ArrayRef<Type>{},
                                ArrayRef<Value>{});
         Operation *term = thenBlock.getTerminator();
-        OpBuilder b2(term);
+        OpBuilder b2(ctx);
+        b2.setInsertionPoint(term);
         func::FuncOp fend = getOrCreateFunc("trace_condition_then_end");
-        b2.create<func::CallOp>(loc, fend.getSymName(), ArrayRef<Type>{},
+        b2.create<func::CallOp>(loc, fend.getName(), ArrayRef<Type>{},
                                 ArrayRef<Value>{});
       }
-      if (ifOp.elseRegion().empty() == false) {
-        Block &elseBlock = ifOp.elseRegion().front();
+      Region &elseReg = ifOp.getElseRegion();
+      if (!elseReg.empty()) {
+        Block &elseBlock = elseReg.front();
         OpBuilder b(&elseBlock, elseBlock.begin());
         func::FuncOp febegin = getOrCreateFunc("trace_condition_else_begin");
-        b.create<func::CallOp>(loc, febegin.getSymName(), ArrayRef<Type>{},
+        b.create<func::CallOp>(loc, febegin.getName(), ArrayRef<Type>{},
                                ArrayRef<Value>{});
         Operation *term = elseBlock.getTerminator();
-        OpBuilder b2(term);
+        OpBuilder b2(ctx);
+        b2.setInsertionPoint(term);
         func::FuncOp feend = getOrCreateFunc("trace_condition_else_end");
-        b2.create<func::CallOp>(loc, feend.getSymName(), ArrayRef<Type>{},
+        b2.create<func::CallOp>(loc, feend.getName(), ArrayRef<Type>{},
                                 ArrayRef<Value>{});
       }
     }
-    for (auto ifOp : module.getOps<AffineIfOp>()) {
+    for (auto ifOp : module.getOps<mlir::affine::AffineIfOp>()) {
       Location loc = ifOp.getLoc();
-      if (ifOp.thenRegion().empty() == false) {
-        Block &thenBlock = ifOp.thenRegion().front();
+      Region &thenReg = ifOp.getThenRegion();
+      if (!thenReg.empty()) {
+        Block &thenBlock = thenReg.front();
         OpBuilder b(&thenBlock, thenBlock.begin());
         func::FuncOp fbegin = getOrCreateFunc("trace_condition_then_begin");
-        b.create<func::CallOp>(loc, fbegin.getSymName(), ArrayRef<Type>{},
+        b.create<func::CallOp>(loc, fbegin.getName(), ArrayRef<Type>{},
                                ArrayRef<Value>{});
         Operation *term = thenBlock.getTerminator();
-        OpBuilder b2(term);
+        OpBuilder b2(ctx);
+        b2.setInsertionPoint(term);
         func::FuncOp fend = getOrCreateFunc("trace_condition_then_end");
-        b2.create<func::CallOp>(loc, fend.getSymName(), ArrayRef<Type>{},
+        b2.create<func::CallOp>(loc, fend.getName(), ArrayRef<Type>{},
                                 ArrayRef<Value>{});
       }
-      if (ifOp.elseRegion().empty() == false) {
-        Block &elseBlock = ifOp.elseRegion().front();
+      Region &elseReg = ifOp.getElseRegion();
+      if (!elseReg.empty()) {
+        Block &elseBlock = elseReg.front();
         OpBuilder b(&elseBlock, elseBlock.begin());
         func::FuncOp febegin = getOrCreateFunc("trace_condition_else_begin");
-        b.create<func::CallOp>(loc, febegin.getSymName(), ArrayRef<Type>{},
+        b.create<func::CallOp>(loc, febegin.getName(), ArrayRef<Type>{},
                                ArrayRef<Value>{});
         Operation *term = elseBlock.getTerminator();
-        OpBuilder b2(term);
+        OpBuilder b2(ctx);
+        b2.setInsertionPoint(term);
         func::FuncOp feend = getOrCreateFunc("trace_condition_else_end");
-        b2.create<func::CallOp>(loc, feend.getSymName(), ArrayRef<Type>{},
+        b2.create<func::CallOp>(loc, feend.getName(), ArrayRef<Type>{},
                                 ArrayRef<Value>{});
       }
     }
@@ -90,7 +101,7 @@ MLIR_DECLARE_EXPLICIT_TYPE_ID(ExamplePass)
 MLIR_DEFINE_EXPLICIT_TYPE_ID(ExamplePass)
 
 mlir::PassPluginLibraryInfo getFunctionCallCounterPassPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "ExamplePass", "1.0",
+  return {MLIR_PLUGIN_API_VERSION, "yurkin-condition-tracer", "1.0",
           []() { mlir::PassRegistration<ExamplePass>(); }};
 }
 
